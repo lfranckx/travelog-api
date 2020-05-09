b0ede49fe1f705a25a7f0d31ba974ffda1c26a79
Bowie <bowie@thinkful.com>
2020-05-09 09:30:28 -0700

    finish auth-router

--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
 
+const app = express();
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+const errorHandler = require('./errorHandler');
+const authRouter = require('./auth/auth-router');
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
-})
+    res.send('Hello, boilerplate!');
+});
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error'}}
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+app.use('/api/auth', authRouter);
 
-module.exports = app
+app.use(errorHandler);
+
+module.exports = app;
---
+++ src/auth-router/auth-router.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const AuthService = require('./auth-service');
+const { requireAuth } = require('../middleware/jwt-auth');
+
+const jsonBodyParser = express.json();
+const authRouter = express.Router();
+
+authRouter
+    .post('/login', jsonBodyParser, (req, res, next) => {
+        const { username, password } = req.body;
+        const loginUser = { username, password };
+
+        for (const [key, value] of Object.entries(loginUser))
+            if (value == null)
+                return res.status(400).json({
+                    error: `Missing '${key} in request body`
+                });
+        AuthService.getUserWithUsername(
+            req.app.get('db'),
+            loginUser.username
+        )
+            .then(dbUser => {
+                if (!dbUser)
+                    return res.status(400).json({
+                        error: 'Incorrect username or password'
+                    });
+                
+                    return AuthService.comparePasswords(loginUser.password, dbUser.password)
+                        .then(compareMatch => {
+                            if (!compareMatch)
+                            return res.status(400).json({
+                                error: 'Incorrect username or password',
+                            });
+
+                            const sub = dbUser.username;
+                            const payload = { user_id: dbUser.id };
+                            res.send({
+                                authToken: AuthService.createJwt(sub, payload)
+                            });
+                        });
+            })
+            .catch(next);
+    });
+
+authRouter
+    .post('/refresh', requireAuth, (req, res) => {
+        const sub = req.user.username;
+        const payload = { user_id: req.user.id };
+        res.send({
+            authToken: AuthService.createJwt(sub, payload),
+        });
+    });
+
+module.exports = authRouter
