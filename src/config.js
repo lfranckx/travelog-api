7ed93bb190b68bb5dd8306c931ea83243a7f03aa
Bowie <bowie@thinkful.com>
2020-05-09 10:14:55 -0700

    add jwt-auth

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-}
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lachlan@localhost/ratemybaby',
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
+    awsSecretAccess: process.env.awsSecretAccess || 'aws-secret-access',
+    awsAccessKeyId: process.env.awsAccessKeyId || 'aws-key-id',
+    awsRegion: process.env.awsRegion || 'aws-region',
+    bucket: process.env.bucket || 'bucket'
+};
