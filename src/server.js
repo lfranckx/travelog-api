967dc78ecef87295edf635f41f6cefc28d96cca5
Bowie <bowie@thinkful.com>
2020-05-09 17:11:24 -0700

    add postgrator-config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const app = require('./app')
-const { PORT } = require('./config')
+const app = require('./app');
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
