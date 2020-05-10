a3bdbb69e52cb23fcad80e8d88f18a3fab2626bc
Bowie <bowie@thinkful.com>
2020-05-09 17:06:44 -0700

    add dependencies package.json

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lachlan@localhost/ratemybaby',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lachlan@localhost/travelog',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
     awsSecretAccess: process.env.awsSecretAccess || 'aws-secret-access',
