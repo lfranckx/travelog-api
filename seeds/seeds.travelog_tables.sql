7aeb333ddd1ae8c0c196185e03d0df1d355ca5ad
Bowie <bowie@thinkful.com>
2020-05-10 19:48:17 -0700

    update seeds

--- seeds/seeds.travelog_tables.sql
+++ seeds/seeds.travelog_tables.sql
@@ -68,31 +68,31 @@ VALUES
     (
         'Mike Sherman', 
         'Donec finibus neque quis est molestie, id volutpat metus auctor. Aliquam augue ante, ultrices ac lacinia non, vulputate quis nisl. Nullam vitae tempus urna. Integer nec cursus elit.',
-        'https://loremflickr.com/750/300/paris,girl/random=1',
+        'https://loremflickr.com/750/300/person?random=1',
         1
     ),
     (
         'Deb Straus',
         'Donec finibus neque quis est molestie, id volutpat metus auctor. Aliquam augue ante, ultrices ac lacinia non, vulputate quis nisl. Nullam vitae tempus urna. Integer nec cursus elit.',
-        'https://loremflickr.com/750/300/paris,girl/random=2',
+        'https://loremflickr.com/750/300/person?random=2',
         2
     ),
     (
         'Joe Perry',
         'Donec finibus neque quis est molestie, id volutpat metus auctor. Aliquam augue ante, ultrices ac lacinia non, vulputate quis nisl. Nullam vitae tempus urna. Integer nec cursus elit.',
-        'https://loremflickr.com/750/300/paris,girl/random=3',
+        'https://loremflickr.com/750/300/person?random=3',
         3
     ),
     (
         'Stan Francisco',
         'Donec finibus neque quis est molestie, id volutpat metus auctor. Aliquam augue ante, ultrices ac lacinia non, vulputate quis nisl. Nullam vitae tempus urna. Integer nec cursus elit.',
-        'https://loremflickr.com/750/300/paris,girl/random=4',
+        'https://loremflickr.com/750/300/person?random=4',
         4
     ),
     (
         'Warm Milk',
         'Donec finibus neque quis est molestie, id volutpat metus auctor. Aliquam augue ante, ultrices ac lacinia non, vulputate quis nisl. Nullam vitae tempus urna. Integer nec cursus elit.',
-        'https://loremflickr.com/750/300/paris,girl/random=5',
+        'https://loremflickr.com/750/300/person?random=5',
         5
     );
 
