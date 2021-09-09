339276b181869dc9f65c45c183a27379f4f61cdb
Bowie <lfranckx@thinkful.com>
2021-09-08 23:05:48 -0500

    add test database url and quotations around database url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://lachlan@localhost/travelog',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lachlan@localhost/travelog-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
     awsSecretAccess: process.env.awsSecretAccess || 'aws-secret-access',
